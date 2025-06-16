e85ec38c0c7bcbd9974f53a3fd3fcf06a8c79f4a
sanjayrp2 <sanjayramesh.official@gmail.com>
2025-06-16 12:40:45 +0530

    second changes

--- model-login/src/Login.tsx
+++ model-login/src/Login.tsx
@@ -49,20 +49,19 @@ const Login = () => {
 
     return (
         <div className='w-full h-screen flex'>
-            {/* Left half of the screen - background styling */}
+        
             <div className='w-1/2 h-full flex flex-col bg-[#282c34] items-center justify-center'>
             </div>
 
-            {/* Right half of the screen - login form */}
+            
             <div className='w-1/2 h-full bg-[#1a1a1a] flex flex-col p-20 justify-center'>
                 <div className='w-full flex flex-col max-w-[450px] mx-auto'>
-                    {/* Header section with title and welcome message */}
+                  
                     <div className='w-full flex flex-col mb-10 text-white'>
                         <h3 className='text-4xl font-bold mb-2'>Login</h3>
                         <p className='text-lg mb-4'>Welcome Back! Please enter your details.</p>
                     </div>
 
-                    {/* Input fields for email and password */}
                     <div className='w-full flex flex-col mb-6'>
                         <input
                             type='email'
@@ -78,7 +77,6 @@ const Login = () => {
                             onChange={(e) => setPassword(e.target.value)} />
                     </div>
 
-                    {/* Button to log in with email and password */}
                     <div className='w-full flex flex-col mb-4'>
                         <button
                             className='w-full bg-transparent border border-white text-white my-2 font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
@@ -88,16 +86,15 @@ const Login = () => {
                         </button>
                     </div>
 
-                    {/* Display error message if there is one */}
+                   
                     {error && <div className='text-red-500 mb-4'>{error}</div>}
 
-                    {/* Divider with 'OR' text */}
+                   
                     <div className='w-full flex items-center justify-center relative py-4'>
                         <div className='w-full h-[1px] bg-gray-500'></div>
                         <p className='text-lg absolute text-gray-500 bg-[#1a1a1a] px-2'>OR</p>
                     </div>
 
-                    {/* Button to log in with Google */}
                     <button
                         className='w-full bg-white text-black font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer mt-7'
                         onClick={signInWithGoogle}
@@ -106,7 +103,7 @@ const Login = () => {
                     </button>
                 </div>
 
-                {/* Link to sign up page */}
+          
                 <div className='w-full flex items-center justify-center mt-10'>
                     <p className='text-sm font-normal text-gray-400'>Don't have an account? <span className='font-semibold text-white cursor-pointer underline'><a href='/signup'>Sign Up</a></span></p>
                 </div>
--- model-login/src/Signup.tsx
+++ model-login/src/Signup.tsx
@@ -3,22 +3,20 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPas
 import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    // Initialize Firebase authentication and navigation
     const auth = getAuth();
     const navigate = useNavigate();
-    
-    // State variables for managing authentication state, email, password, confirm password, and error messages
+  
     const [authing, setAuthing] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
 
-    // Function to handle sign-up with Google
+  
     const signUpWithGoogle = async () => {
         setAuthing(true);
         
-        // Use Firebase to sign up with Google
+
         signInWithPopup(auth, new GoogleAuthProvider())
             .then(response => {
                 console.log(response.user.uid);
@@ -30,9 +28,9 @@ const Signup = () => {
             });
     };
 
-    // Function to handle sign-up with email and password
+    
     const signUpWithEmail = async () => {
-        // Check if passwords match
+        
         if (password !== confirmPassword) {
             setError('Passwords do not match');
             return;
@@ -41,7 +39,7 @@ const Signup = () => {
         setAuthing(true);
         setError('');
 
-        // Use Firebase to create a new user with email and password
+        
         createUserWithEmailAndPassword(auth, email, password)
             .then(response => {
                 console.log(response.user.uid);
@@ -56,20 +54,20 @@ const Signup = () => {
 
     return (
         <div className='w-full h-screen flex'>
-            {/* Left half of the screen - background styling */}
+           
             <div className='w-1/2 h-full flex flex-col bg-[#282c34] items-center justify-center'>
             </div>
 
-            {/* Right half of the screen - signup form */}
+            
             <div className='w-1/2 h-full bg-[#1a1a1a] flex flex-col p-20 justify-center'>
                 <div className='w-full flex flex-col max-w-[450px] mx-auto'>
-                    {/* Header section with title and welcome message */}
+                    
                     <div className='w-full flex flex-col mb-10 text-white'>
                         <h3 className='text-4xl font-bold mb-2'>Sign Up</h3>
                         <p className='text-lg mb-4'>Welcome! Please enter your information below to begin.</p>
                     </div>
 
-                    {/* Input fields for email, password, and confirm password */}
+                   
                     <div className='w-full flex flex-col mb-6'>
                         <input
                             type='email'
@@ -94,10 +92,10 @@ const Signup = () => {
                         />
                     </div>
 
-                    {/* Display error message if there is one */}
+          
                     {error && <div className='text-red-500 mb-4'>{error}</div>}
 
-                    {/* Button to sign up with email and password */}
+                    
                     <div className='w-full flex flex-col mb-4'>
                         <button
                             onClick={signUpWithEmail}
@@ -107,13 +105,12 @@ const Signup = () => {
                         </button>
                     </div>
 
-                    {/* Divider with 'OR' text */}
+
                     <div className='w-full flex items-center justify-center relative py-4'>
                         <div className='w-full h-[1px] bg-gray-500'></div>
                         <p className='text-lg absolute text-gray-500 bg-[#1a1a1a] px-2'>OR</p>
                     </div>
 
-                    {/* Button to sign up with Google */}
                     <button
                         onClick={signUpWithGoogle}
                         disabled={authing}
@@ -122,7 +119,7 @@ const Signup = () => {
                     </button>
                 </div>
 
-                {/* Link to login page */}
+           
                 <div className='w-full flex items-center justify-center mt-10'>
                     <p className='text-sm font-normal text-gray-400'>Already have an account? <span className='font-semibold text-white cursor-pointer underline'><a href='/login'>Log In</a></span></p>
                 </div>
